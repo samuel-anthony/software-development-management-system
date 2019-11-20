9282fbb9e3593d2f825ea4a67b838517c6848e36
minatorisato <36133909+minatorisato@users.noreply.github.com>
2019-11-20 11:40:47 +0700

    navbar
    cman nambah navbar doang

--- projectManagement/resources/views/auth/login.blade.php
+++ projectManagement/resources/views/auth/login.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.customnavbar')
 
 @section('content')
 <div class="container">
---
+++ projectManagement/resources/views/layouts/customnavbar.blade.php
@@ -0,0 +1,70 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+</head>
+<body>
+    <nav class="nav-wrapper">
+        <div class="container">
+            <a href="#" class="brand-logo">{{config('app.name')}}</a>
+            @auth    
+            <a href="#" data-target="menu-responsive" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+                 <ul class="right hide-on-med-and-down">
+                        <li>
+                            <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                            </a>
+
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                @csrf
+                            </form>
+                        </li>
+                </ul>
+            @endauth    
+        </div>
+    </nav>
+    
+    <ul id="menu-responsive" class="sidenav">
+                        <li>
+                            <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                            </a>
+
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                @csrf
+                            </form>
+                        </li>
+                </ul>
+
+    <main class="py-4">
+            @yield('content')
+    </main>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <script>
+        const slide_menu = document.querySelectorAll(".sidenav");
+        M.Sidenav.init(slide_menu,{});
+    </script>
+</body>
+</html>
