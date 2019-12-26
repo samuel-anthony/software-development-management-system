cc043a556c20daaa5eb114c915ecc068f361deb7
aldowjy <aldowjy12@gmail.com>
2019-12-26 20:28:58 +0700

    Deleting Image Assets, and Fixing Style Hoa

--- projectManagement/resources/views/hoa/index.blade.php
+++ projectManagement/resources/views/hoa/index.blade.php
@@ -2,55 +2,40 @@
 
 @section('content')
 <div class="container">
-    
-<div class="content">
-        <div class="row">
-          <div class="col-md-12">
-            <div class="card">
-              <div class="card-header">
-                <h4 class="card-title"> Simple Table</h4>
+      <div class="row justify-content-center">
+          <div class="col-md-10">
+              <div class="card py-3 px-4">
+                  <div class="card-header text-center"><h3 class="font-weight-bold">New User Request</h3></div>
+                      <div class="card-body">
+                          <div class="table-responsive">
+                              <table class="table">
+                                  <thead class="text-primary">
+                                      <th>No.</th>
+                                      <th>Action type</th>
+                                      <th>Detail</th>
+                                      <th class="text-center">Action</th>
+                                  </thead>
+                                  <tbody>
+                                      @php($num = 1)
+                                      @foreach ($requestAdmins as $requestAdmin)
+                                      <tr>
+                                          <td>{{$num}}</td>
+                                          <td>{{$requestAdmin->type}}</td>
+                                          <td>Username : {{$requestAdmin->data->user_name}}, email :
+                                              {{$requestAdmin->data->email}}</td>
+                                          <td class="text-center">
+                                              <a href='{{$prefix}}/detail/{{$requestAdmin->id}}'><button type="submit" class="btn btn-primary">View</button></a>
+                                          </td>
+                                          @php($num++)
+                                      </tr>
+                                      @endforeach
+                                  </tbody>
+                              </table>
+                          </div>
+                      </div>
+                  </div>
               </div>
-              <div class="card-body">
-                <div class="table-responsive">
-                  <table class="table">
-                    <thead class=" text-primary">
-                      <th>
-                        No.
-                      </th>
-                      <th>
-                        Action Type
-                      </th>
-                      <th>
-                        Detail
-                      </th>
-                      <th class="text-center">
-                        Action
-                      </th>
-                    </thead>
-                    <tbody>
-                    @php($num = 1)
-                    @foreach ($requestAdmins as $requestAdmin)
-                        <tr>
-                            <td>{{$num}}</td>
-                            <td>{{$requestAdmin->type}}</td>
-                            <td>Username : {{$requestAdmin->data->user_name}}, email : {{$requestAdmin->data->email}}</td>
-                            <td class="text-center">
-                              <a href='{{$prefix}}/detail/{{$requestAdmin->id}}'><button type="submit" class="btn btn-primary">View</button></a>
-                            </td>
-                            @php($num++)
-                        </tr>    
-                    @endforeach
-                      
-                    </tbody>
-                  </table>
-
-
-                </div>
-              </div>
-            </div>
-          </div>
         </div>
-      </div>
-      
+    </div>
 </div>
 @endsection
--- projectManagement/resources/views/layouts/customlayout.blade.php
+++ projectManagement/resources/views/layouts/customlayout.blade.php
@@ -2,196 +2,136 @@
 <html lang="en">
 
 <head>
-  <meta charset="utf-8" />
-  <link rel="apple-touch-icon" sizes="76x76" href="/assets/img/apple-icon.png">
-  <link rel="icon" type="image/png" href="/assets/img/favicon.png">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-  <title>{{ config('app.name', 'Laravel') }}</title>
-  <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
-  <!--     Fonts and icons     -->
-  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-  <!-- CSS Files -->
-  <link href="/assets/css/bootstrap.min.css" rel="stylesheet" />
-  <link href="/assets/css/now-ui-dashboard.css?v=1.3.0" rel="stylesheet" />
-  <!-- CSS Just for demo purpose, don't include it in your project -->
-  <link href="/assets/demo/demo.css" rel="stylesheet" />
-  
-  @if(is_null($allMenu ?? null))  
-  <style>
-    html,
-    body {
-      height: 100%;
-    }
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Brand Advertising Management System</title>
+    <link href="/assets/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="/assets/css/now-ui-dashboard.css?v=1.3.0" rel="stylesheet" />
+    <link href="/assets/css/main.css" rel="stylesheet" />
 
-    body {
-      display: -ms-flexbox;
-      display: flex;
-      -ms-flex-align: center;
-      align-items: center;
-      padding-top: 40px;
-      padding-bottom: 40px;
-      background-color: #f5f5f5;
-    }
+    @if(is_null($allMenu ?? null))  
+      <style>
+        html,
+        body {
+          height: 100%;
+        }
 
-    .form-signin {
-      width: 100%;
-      max-width: 330px;
-      padding: 15px;
-      margin: auto;
-    }
-    .form-signin .checkbox {
-      font-weight: 400;
-    }
-    .form-signin .form-control {
-      position: relative;
-      box-sizing: border-box;
-      height: auto;
-      padding: 10px;
-      font-size: 16px;
-    }
-    .form-signin .form-control:focus {
-      z-index: 2;
-    }
-    .form-signin input[type="email"] {
-      margin-bottom: -1px;
-      border-bottom-right-radius: 0;
-      border-bottom-left-radius: 0;
-    }
-    .form-signin input[type="password"] {
-      margin-bottom: 10px;
-      border-top-left-radius: 0;
-      border-top-right-radius: 0;
-    }
-</style>
-@endif
+        body {
+          display: -ms-flexbox;
+          display: flex;
+          -ms-flex-align: center;
+          align-items: center;
+          padding-top: 40px;
+          padding-bottom: 40px;
+          background-color: #f5f5f5;
+        }
+
+        .form-signin {
+          width: 100%;
+          max-width: 330px;
+          padding: 15px;
+          margin: auto;
+        }
+        .form-signin .checkbox {
+          font-weight: 400;
+        }
+        .form-signin .form-control {
+          position: relative;
+          box-sizing: border-box;
+          height: auto;
+          padding: 10px;
+          font-size: 16px;
+        }
+        .form-signin .form-control:focus {
+          z-index: 2;
+        }
+        .form-signin input[type="email"] {
+          margin-bottom: -1px;
+          border-bottom-right-radius: 0;
+          border-bottom-left-radius: 0;
+        }
+        .form-signin input[type="password"] {
+          margin-bottom: 10px;
+          border-top-left-radius: 0;
+          border-top-right-radius: 0;
+        }
+    </style>
+    @endif
 </head>
 
-<body class="">
-  @if(!is_null($allMenu ?? null))  
-  <div class="wrapper">
-    <div class="sidebar" data-color="blue">
-      <!--
-        Tip 1: You can change the color of the sidebar using: data-color="blue | green | orange | red | yellow"
-    -->
-      <div class="logo">
-        <a href="/home" class="simple-text logo-normal">
-          Home
-        </a>
-      </div>
-      <div class="sidebar-wrapper" id="sidebar-wrapper">
-        <ul class="nav">
-          @for($counter = 0; $counter < count($allMenu); $counter++)  
-            <li>
-              <a href="{{$prefix ?? null}}/{{$allMenu[$counter]->menu_name}}">
-                <i class="now-ui-icons design_app"></i>
-                <p>{{$allMenu[$counter]->menu_name}}</p>
-              </a>
-            </li>
-          @endfor
-          <li class="active-pro">
-            <a href="./upgrade.html">
-              <i class="now-ui-icons arrows-1_cloud-download-93"></i>
-              <p>Upgrade to PRO</p>
-            </a>
-          </li>
-        </ul>
-      </div>
-    </div>
-    <div class="main-panel" id="main-panel">
-      <!-- Navbar -->
-      <nav class="navbar navbar-expand-lg navbar-transparent  bg-primary  navbar-absolute">
-        <div class="container-fluid">
-          <div class="navbar-wrapper">
-            <div class="navbar-toggle">
-              <button type="button" class="navbar-toggler">
-                <span class="navbar-toggler-bar bar1"></span>
-                <span class="navbar-toggler-bar bar2"></span>
-                <span class="navbar-toggler-bar bar3"></span>
-              </button>
-            </div>
-            <a class="navbar-brand" href="#pablo">{{$menuName ?? 'Home'}}</a>
-          </div>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-bar navbar-kebab"></span>
-            <span class="navbar-toggler-bar navbar-kebab"></span>
-            <span class="navbar-toggler-bar navbar-kebab"></span>
-          </button>
-          <div class="collapse navbar-collapse justify-content-end" id="navigation">
-            <form>
-              <div class="input-group no-border">
-                <input type="text" value="" class="form-control" placeholder="Search...">
-                <div class="input-group-append">
-                  <div class="input-group-text">
-                    <i class="now-ui-icons ui-1_zoom-bold"></i>
-                  </div>
-                </div>
+<body>
+    @if(!is_null($allMenu ?? null))
+      <div class="wrapper">
+          <div class="sidebar" data-color="blue">
+              <div class="logo">
+                  <a href="/home" class="simple-text logo-normal">Home</a>
               </div>
-            </form>
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <a class="nav-link" href="#pablo">
-                  <i class="now-ui-icons media-2_sound-wave"></i>
-                  <p>
-                    <span class="d-lg-none d-md-block">Stats</span>
-                  </p>
-                </a>
-              </li>
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  <i class="now-ui-icons users_single-02"></i>
-                  <p>
-                    <span class="d-md-block">{{Auth::user()->first_name}}</span>
-                  </p>
-                </a>
-                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
-                  <a class="dropdown-item" href="/user/detail user/{{Auth::user()->id}}">Edit Profile</a>
-                  <a class="dropdown-item" href="{{ route('logout') }}" 
-                    onclick="event.preventDefault();document.getElementById('logout-form').submit();">
-                    Logout
-                  </a>
-                  <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                </div>
-              </li>
-            </ul>
+              <div class="sidebar-wrapper" id="sidebar-wrapper">
+                  <ul class="nav">
+                      @for($counter = 0; $counter < count($allMenu); $counter++)
+                      <li>
+                        <a href="{{$prefix ?? null}}/{{$allMenu[$counter]->menu_name}}">
+                            <i class="now-ui-icons design_app"></i>
+                            <p>{{$allMenu[$counter]->menu_name}}</p>
+                        </a>
+                      </li>
+                      @endfor
+                  </ul>
+              </div>
           </div>
-        </div>
-      </nav>
-      <!-- End Navbar -->
-      <div class="panel-header panel-header-sm">
+          <div class="main-panel" id="main-panel">
+              <nav class="navbar navbar-expand-lg navbar-transparent bg-primary navbar-absolute">
+                  <div class="container-fluid">
+                      <div class="navbar-wrapper">
+                          <div class="navbar-toggle">
+                              <button type="button" class="navbar-toggler">
+                                  <span class="navbar-toggler-bar bar1"></span>
+                                  <span class="navbar-toggler-bar bar2"></span>
+                                  <span class="navbar-toggler-bar bar3"></span>
+                              </button>
+                          </div>
+                          <p class="navbar-brand">{{$menuName ?? 'Dashboard'}}</p>
+                      </div>
+                      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
+                          <span class="navbar-toggler-bar navbar-kebab"></span>
+                          <span class="navbar-toggler-bar navbar-kebab"></span>
+                          <span class="navbar-toggler-bar navbar-kebab"></span>
+                      </button>
+                      <div class="collapse navbar-collapse justify-content-end" id="navigation">
+                          <ul class="navbar-nav">
+                              <li class="nav-item dropdown">
+                                  <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                      <i class="now-ui-icons users_single-02"></i>
+                                      <p>{{Auth::user()->first_name}}</p>
+                                  </a>
+                                  <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
+                                      <a class="dropdown-item" href="/user/detail user/{{Auth::user()->id}}">Edit Profile</a>
+                                      <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();"> Logout </a>
+                                      <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">@csrf</form>
+                                  </div>
+                              </li>
+                          </ul>
+                      </div>
+                  </div>
+              </nav>
+              <div class="panel-header panel-header-sm"></div>
+              <div class="content">
+                  <div class="row">@yield('content')</div>
+              </div>
+          </div>
       </div>
-      <div class="content">
-        <div class="row">
-          @yield('content')
-        </div>
+    @else
+      <div class="container">
+          <div class="text-center">@yield('content')</div>
       </div>
-      
-    </div>
-  </div>
-  @else
-  <div class="container">
-    <div class="text-center">
-          @yield('content')
-    </div>
-  </div>
-  @endif
-  <!--   Core JS Files   -->
-  <script src="/assets/js/core/jquery.min.js"></script>
-  <script src="/assets/js/core/popper.min.js"></script>
-  <script src="/assets/js/core/bootstrap.min.js"></script>
-  <script src="/assets/js/plugins/perfect-scrollbar.jquery.min.js"></script>
-  <!--  Google Maps Plugin    -->
-  <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
-  <!-- Chart JS -->
-  <script src="/assets/js/plugins/chartjs.min.js"></script>
-  <!--  Notifications Plugin    -->
-  <script src="/assets/js/plugins/bootstrap-notify.js"></script>
-  <!-- Control Center for Now Ui Dashboard: parallax effects, scripts for the example pages etc -->
-  <script src="/assets/js/now-ui-dashboard.min.js?v=1.3.0" type="text/javascript"></script>
-  <!-- Now Ui Dashboard DEMO methods, don't include it in your project! -->
-  <script src="/assets/demo/demo.js"></script>
-</body>
+    @endif
 
+    <script src="/assets/js/core/jquery.min.js"></script>
+    <script src="/assets/js/core/popper.min.js"></script>
+    <script src="/assets/js/core/bootstrap.min.js"></script>
+    <script src="/assets/js/plugins/perfect-scrollbar.jquery.min.js"></script>
+    <script src="/assets/js/plugins/chartjs.min.js"></script>
+    <script src="/assets/js/plugins/bootstrap-notify.js"></script>
+    <script src="/assets/js/now-ui-dashboard.min.js?v=1.3.0" type="text/javascript"></script>
+</body>
 </html>
